287d74042db047f6b1b5c53418bab21dc8d7843c
Knepper <knepperd@wit.edu>
2018-11-27 19:31:41 -0500

    this is the updated cliser

--- tcpCliSer.py
+++ tcpCliSer.py
@@ -1,24 +1,13 @@
 from socket import *
 serverName = '127.0.0.1'
 serverPort = 12000
-I = input("Client or Server")
-if (I == "Client"):
-    clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect((serverName,serverPort))
-    while True:
-        sentence = input('Input: ')
-        clientSocket.send(sentence.encode())
-        if sentence == "end":
-            exit()
-        modifiedSentence = clientSocket.recv(1024).decode()
-        print('From Server:' + modifiedSentence)
-    
-elif (I == "Server"):    
+def sListen():
     serverSocket = socket(AF_INET,SOCK_STREAM)
     serverSocket.bind((serverName,serverPort))
     serverSocket.listen(1)
     print ('The server is ready to receive')
     connectionSocket, addr = serverSocket.accept()
+    print ('connected')
     while True:
         client = connectionSocket.recv(1024).decode()
         if client == "end":
@@ -26,4 +15,24 @@ elif (I == "Server"):
         print('Client:' + client)
         sentence = input('Server: ')
         connectionSocket.send(sentence.encode())
+        
+def sendFile():
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((serverName,serverPort))
+    print ('connected')
+    while True:
+        sentence = input('Input: ')
+        clientSocket.send(sentence.encode())
+        if sentence == "end":
+            exit()
+        modifiedSentence = clientSocket.recv(1024).decode()
+        print('From Server:' + modifiedSentence)
+       
+        
+I = input("Client or Server")
+if (I == "Client"):
+    sendFile()
+    
+elif (I == "Server"):    
+    sListen()
 
