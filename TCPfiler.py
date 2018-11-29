3a5d058facea5a783fdecb1304032bf0541a74e4
Cynler <lucynthi@gmail.com>
2018-11-29 01:19:14 -0500

    Okay version

--- TCPfiler.py
+++ TCPfiler.py
@@ -6,68 +6,69 @@ myIP = socket.gethostbyname(socket.gethostname())
 print(myIP)
 serverPort = 12000
 
-def accept(conn):
-    conn.send("accept".encode())
-    while True:
-        with open('received_file', 'wb') as f:
-            print('file opened')
-            while True:
-                print('receiving data...')
-                data = conn.recv(1024)
-                if data.decode() != "done":
-                    print(data)
-                else:
-                    break
-                # write data to a file
-                f.write(data)
-        f.close()
-        print("successfully got the file")
-
-def deny(conn):
-    conn.send("deny".encode())
-    conn.close()
-
 def sListen():
     serverName = myIP
     serverSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    serverSocket.bind((serverName,12000))
+    serverSocket.bind((serverName,serverPort))
     serverSocket.listen(5)
     print("serve is listening...")
+    
     print ('The server is ready to receive')
     conn, addr = serverSocket.accept()
-    print ('connected ' + serverName + ":" + str(12000))
-    incommingFile = conn.recv(1024).decode()
-    return conn, incommingFile
+    print ('connected ' + serverName + ":" + str(serverPort))
+    while True:
+        client = conn.recv(1024).decode()
+        if client == "end":
+            sendFile.close()
+        if client == "sendFile":
     
-
-def connect(IP):
-    serverName = IP
+            with open('received_file', 'wb') as f:
+                print('file opened')
+                while True:
+                    print('receiving data...')
+                    data = conn.recv(1024)
+                    if data.decode() != "done":
+                        print(data)
+                    else:
+                        break
+                    # write data to a file
+                    f.write(data)
+            f.close()
+            print("successfully got the file")
+        else:
+            print('Client:' + client)
+            sentence = input('Server: ')
+            conn.send(sentence.encode())
+        
+def sendFile():
+    serverName = input("Enter the IP you're connecting to:")
     clientSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    clientSocket.connect((serverName,12000))
-    print ('connected to ' + serverName + ":" + str(12000))
-    return clientSocket
-         
-def sendFile(filePath,FileName,clientSocket):
+    clientSocket.connect((serverName,serverPort))
+    print ('connected to ' + serverName + ":" + str(serverPort))
     bFileFound = 0
     
     while True:
-        fileP = filePath
-        sFileName = FileName
+        sentence = input('Input: ')
+        clientSocket.send(sentence.encode())
+        if sentence == "end":
+            sendFile().close()
+        elif sentence == "sendFile":
+            fileP = input("enter file path")
+            sFileName = input("enter file name")
+            sentence = sFileName
+            clientSocket.send(sentence.encode())
+            
+            for file in os.listdir(fileP):
+                if file == sFileName:
+                    bFileFound = 1
+                    break
         
-        for file in os.listdir(fileP):
-            if file == sFileName:
-                bFileFound = 1
-                break 
-                
-        if bFileFound == 0:
-            print(sFileName + " Not Found in Directory")
-        else:
-            clientSocket.send(FileName.encode())
-            modifiedSentence = clientSocket.recv(1024).decode()
-            if modifiedSentence == 'accept':
-                
-                print(fileP + "/" + sFileName + " File Found")
-                fUploadFile = open(sFileName, "rb")
+            if bFileFound == 0:
+                print(sFileName + " Not Found in Directory")
+        
+            else:
+                print(sFileName + " File Found")
+                fUploadFile = open(fileP + "/" +sFileName, "rb")
                 sRead = fUploadFile.read(1024)
                 count = 1
                 while sRead:
@@ -77,14 +78,14 @@ def sendFile(filePath,FileName,clientSocket):
                     count += 1
                 print("Sending Completed")
                 clientSocket.send("done".encode())
-                clientSocket.close()
-            else:
-                clientSocket.close()
+
+        modifiedSentence = clientSocket.recv(1024).decode()
+        print('From Server:' + modifiedSentence)
     
 
-I = input("Send File or Receive File: ")
+I = input("Send File or Receive File:")
 if (I.lower() == "send file"):
-    sendFile(filePath, fileName, self.connect(IP))
+    sendFile()
     
 elif (I.lower() == "receive file"):    
     sListen()
